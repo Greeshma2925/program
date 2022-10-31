da70fa3a15b9a088e8e5bc5069079c9679457898
Greeshma2925 <117100293+Greeshma2925@users.noreply.github.com>
2022-10-31 20:52:18 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -123,3 +123,83 @@ class main
         //}
     }
 }
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//nullables
+using System;
+class Program1
+{
+    static void Main()
+    {
+        //int? ticketsonsale = null;
+        //int? ticketsonsale = Convert.ToInt32(Console.ReadLine());
+        //int AvailableTickets;
+        //if (ticketsonsale == null)
+        //{
+
+        //    AvailableTickets = 0;
+        //}
+        //else
+        //{
+        //    AvailableTickets = ticketsonsale ?? 3;
+        //}
+        //Console.WriteLine("AvailableTickets = {0}", AvailableTickets);
+
+        //int? a = null;
+        //int? b = 1234;
+        //Console.WriteLine(a);
+        //Console.WriteLine(b);
+
+        //Datatype conversion
+
+        //float f = 12.2F;
+        //int i = (int)f;
+        //Console.WriteLine(i);
+
+        //String num = "2529";
+        //int i = int.Parse(num);
+        //Console.WriteLine(num);
+
+        //String num1 = "100G";
+        //int j = 0;
+        //int.TryParse(num1, out j);
+        //Console.WriteLine(j);
+
+        //String num1 = Console.ReadLine();
+        //int j = 0;
+        //bool IsConvSuccs = int.TryParse(num1, out j);
+        //if (IsConvSuccs)
+        //{
+        //    Console.WriteLine(j);
+
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Enter a Valid Number");
+        //}
+
+        //ARRAY INTIALIZATION
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int[] PRU = new int[n];
+        ////int i = Convert.ToInt32(Console.ReadLine());
+        //for (int i = 0; i <= n; i++)
+        //{
+        //    PRU[i] = Convert.ToInt32(Console.ReadLine());
+
+        //}
+        //for (int i = 0; i < PRU.Length; i++)
+        //{
+        //    Console.WriteLine(PRU[i].ToString());
+        //}
+
+   }
+}
+
+
+
+
+
+
