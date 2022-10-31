4f01246fae14dc020b85b496fa92637421aea498
Greeshma2925 <117100293+Greeshma2925@users.noreply.github.com>
2022-10-31 20:50:23 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,125 @@
+using System;
+class main
+{
+    static void Main()
+    {
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //int sum = 0;
+        //int dif = a - b;
+        //sum = a + b;
+        //Console.WriteLine(sum);
+        //Console.WriteLine(dif);
+
+        //byte
+        //byte b1 = 0xF7;
+        //byte b2 = 0xAB;
+        //byte temp;
+        //temp = (byte)(b1 & b2);
+        //Console.Write(temp + " ");
+        //temp = (byte)(b1 ^ b2);
+        //Console.WriteLine(temp);
+
+        //ternary
+        //int num;
+        //Console.WriteLine("the value of num is: {0}",num);
+        //bool isnum = num == 25 ? true : false;
+        //Console.WriteLine("Num==25 is {0}", isnum);
+        //string Name = "Ayush\\Kishor\\PascalCase";
+        //Console.WriteLine(Name);`
+
+        //swapping of 2nos
+        //int a = Convert.ToInt32(Console.ReadLine());
+        //int b = Convert.ToInt32(Console.ReadLine());
+        //int temp;
+        //temp = a;
+        //a = b;
+        //b = temp;
+        //Console.WriteLine("THE VALUES OF a & b are: {0},{1}", a, b);
+
+        //FIBONACCI
+        //int a = 0;
+        //int b = 1;
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int fib = 0;
+        //Console.WriteLine("{0},{1}", a, b);
+        //for(int i = 0; i <= n; i++)
+        //{
+        //    fib = a + b;
+        //    a = b;
+        //    b = fib;
+
+        //    Console.WriteLine(fib);
+        //}
+
+        //loops
+        //int age = Convert.ToInt32(Console.ReadLine());
+        //int n= Convert.ToInt32(Console.ReadLine());
+        //for(int i = 0; i <= n; i++)
+        //{
+        //    if (age > n)
+        //    {
+        //        Console.WriteLine("{0}",
+        //        "You look older than you are !");
+        //    }
+        //}
+
+        //pattern[square]
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //for (int i = 0; i < n; i++)
+        //{
+        //    for (int j = 0; j < n; j++)
+        //    {
+        //        Console.Write("@");
+        //    }
+        //    Console.WriteLine(" ");
+        //}
+
+        //factorial
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int fact = 1;
+        //for (int i = 1; i <= n; i++)
+        //{
+        //    fact = fact * i;
+        //    //Console.WriteLine("THE VALUE OF FACT IS:{0}",fact);
+
+        //}
+        //Console.WriteLine("THE VALUE OF FACT IS:{0}", fact);
+
+        //PATTERN1
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //for (int i = 1; i <= n; i++)
+        //{
+        //    for (int j = i; j <= n; j++)
+        //    {
+        //        Console.Write("@");
+        //    }
+        //    Console.WriteLine(" ");
+        //}
+
+        //pattern2
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //for (int i = 0; i <= n; i++)
+        //{
+        //    for (int j = 0; j <= i; j++)
+        //    {
+        //        Console.Write("@");
+        //    }
+        //    Console.WriteLine(" ");
+        //}
+
+        //intialisation of array !
+        //int[] arr = new int[10];
+        //int n = Convert.ToInt32(Console.ReadLine());
+
+        //for (int i = 0; i <= n; i++)
+        //{
+        //    Console.WriteLine(arr[i]);
+        //}
+
+        //for (int i = 0; i <= n; i++)
+        //{
+        //    Console.WriteLine(arr[i]);
+        //}
+    }
+}
